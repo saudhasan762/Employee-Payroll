1516acddabd82b08d152bda064c388b3111d89e3
saudhasan762 <azanali152@gmail.com>
2021-03-08 00:34:31 +0530

    Created Test class to demonstrate File Operations

---
+++ main/java/employee/FileUtils.java
@@ -0,0 +1,15 @@
+package employee;
+
+import java.io.File;
+
+public class FileUtils {
+    public static boolean deleteFiles(File contentsToDelete){
+        File[] allCounts = contentsToDelete.listFiles();
+        if (allCounts != null){
+            for (File file : allCounts) {
+                deleteFiles(file);
+            }
+        }
+        return contentsToDelete.delete();
+    }
+}
