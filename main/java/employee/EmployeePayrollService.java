5d1b2517da2e265519f854ae6dc79274cde7bb2b
saudhasan762 <azanali152@gmail.com>
2021-03-08 08:31:00 +0530

    Storing a Employee Payroll into a File

--- main/java/employee/EmployeePayrollService.java
+++ main/java/employee/EmployeePayrollService.java
@@ -8,29 +8,48 @@ import java.util.Scanner;
 public class EmployeePayrollService {
     private List<EmployeePayrollData> employeePayrollList;
 
+    public enum IOService{CONSOLE_IO,FILE_IO,DB_IO,REST_IO}
+
     public EmployeePayrollService(List<EmployeePayrollData> employeePayrollList){
-        this.employeePayrollList = employeePayrollList;
-    }
-    public static void main(String[] args){
-        ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<>();
-        EmployeePayrollService employeePayRollService = new EmployeePayrollService(employeePayrollList);
-        Scanner consoleInputReader = new Scanner(System.in);
-        employeePayRollService.readEmployeePayrollData(consoleInputReader);
-        employeePayRollService.writeEmployeePayrollData();
+        this.employeePayrollList=employeePayrollList;
     }
 
-    private void readEmployeePayrollData(Scanner consoleInputReader) {
-        System.out.println("Enter Employee ID: ");
-        int id = consoleInputReader.nextInt();
-        System.out.println("Enter Employee Name: ");
-        String name = consoleInputReader.next();
+    public void readData(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter Employee Name");
+        String empName = scan.next();
+        System.out.println("Enter Employee ID");
+        int empID = scan.nextInt();
         System.out.println("Enter Employee Salary");
-        double salary = consoleInputReader.nextDouble();
-        employeePayrollList.add(new EmployeePayrollData(id, name, salary));
+        int empSalary = scan.nextInt();
+
+        EmployeePayrollData adder = new EmployeePayrollData(empID,empName,empSalary);
+        employeePayrollList.add(adder);
     }
 
-    private void writeEmployeePayrollData() {
-        System.out.println("\nWriting Employee Payroll to Console: " +employeePayrollList);
+    public void empWriteData(IOService ioService){
+        if (ioService.equals(IOService.CONSOLE_IO))
+            System.out.println("OutPut\n"+employeePayrollList);
+        else if (ioService.equals(IOService.FILE_IO))
+            new EmployeePayrollFileIOService().writeData(employeePayrollList);
+    }
+
+    public void printData(IOService ioService) {
+        if (ioService.equals(IOService.FILE_IO))
+            new EmployeePayrollFileIOService().printData();
+    }
+
+    public long countEntries(IOService ioService) {
+        if (ioService.equals(IOService.FILE_IO))
+            return new EmployeePayrollFileIOService().countEntries();
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
+        //employeePayrollService.readData();
+        employeePayrollService.empWriteData(IOService.FILE_IO);
     }
 
 }
---
+++ test/java/employee/EmployeePayrollServiceTest.java
@@ -0,0 +1,22 @@
+package employee;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+public class EmployeePayrollServiceTest {
+    @Test
+    void given3EmpWhenWrittenToFilesShouldMatchEmpEntries() {
+        EmployeePayrollData[] arrayOfEmp={
+                new EmployeePayrollData(1,"Jeff Bezos",10000.0),
+                new EmployeePayrollData(2,"Bill Gates",20000.0),
+                new EmployeePayrollData(3,"Mark",150000.0),
+        };
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmp));
+        employeePayrollService.empWriteData(EmployeePayrollService.IOService.FILE_IO);
+        employeePayrollService.printData(EmployeePayrollService.IOService.FILE_IO);
+        long result = employeePayrollService.countEntries(EmployeePayrollService.IOService.FILE_IO);
+        Assertions.assertEquals(3,result);
+    }
+}
